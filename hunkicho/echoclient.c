1180a5313110719b00e73115fcb4cd6bbf30ee27
hunkicho <jgchk4814@gmail.com>
2022-12-12 16:54:48 +0900

    2022-12-12

--- hunkicho/echoclient.c
+++ hunkicho/echoclient.c
@@ -15,12 +15,22 @@ int main(int argc, char **argv)
     port = argv[2];
 
     clientfd = Open_clientfd(host, port);
+    //식별자 clientfd를 rio에 위치한 rio_t 타입의 읽기 버퍼와 연결
+    //(clientfd로 rio_t 구조체를 초기화)
     Rio_readinitb(&rio, clientfd);
 
     while(Fgets(buf, MAXLINE, stdin) != NULL)
     {
+        for(int i=0; i < sizeof(buf) / sizeof(char); i++)
+        {
+            printf("%c",i);
+        }
+        
+        //buf에서 clientfd로 strlen(buf)바이트로 전송
         Rio_writen(clientfd, buf, strlen(buf));
+        // 다음 텍스트 줄을 rio에서 읽고 이것을 buf로 복사하고 텍스트라인을 null로 종료시킨다
         Rio_readlineb(&rio, buf, MAXLINE);
+        // buf가 가지고 있는 문자열을 stdout에 쓴다
         Fputs(buf, stdout);
     }
 
